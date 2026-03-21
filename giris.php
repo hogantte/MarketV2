af8281631fd1e4da48cfaa4f9ac3fec23caa4362
hogantte <doganorhann4021@gmail.com>
2026-03-21 21:18:47 +0300

    Üst Menü Tasarandı.

---
+++ giris.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="post">
+        <span>Login</span>
+
+        <div class="alan">
+
+            <input type="text" placeholder="Kullanıcı Adın">
+        </div>
+
+        <div class="alan">
+
+            <input type="password" placeholder="Parolan">
+        </div>
+
+        
+    </form>
+
+    
+</body>
+
+</html>
