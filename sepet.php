70c24333bb041053ac7f2b03c7a7e3a187707a20
hogantte <doganorhann4021@gmail.com>
2026-04-04 02:55:22 +0300

    Sepet HTML ve CSS kodları tamamlandı örnek ürün eklendi

--- sepet.php
+++ sepet.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="tr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,9 +14,10 @@
         href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
 </head>
+
 <body>
 
-<div class="ustmenu">
+    <div class="ustmenu">
         <a href="index.php">
             <div class="logo"><img src="assets/logo.svg" alt="H" width="32px" height="32px"></div>
             <span>Market</span>
@@ -27,6 +29,29 @@
             <li><a href="#">Hesabım</a></li>
         </ul>
     </div>
-    
+
+
+    <div class="urunler">
+        <div class="sepet">
+            <div class="baslik">
+                <span>Ürünün Adı</span>
+                <span>Ürünün Fiyatı</span>
+            </div>
+            <div class="sepetteki-urun">
+                <span>Kangal</span>
+                <span>200.00 Tl</span>
+                <div class="kaldir"><!-- From Uiverse.io by cssbuttons-io -->
+                    <button class="noselect"><span class="text">Delete</span><span class="icon"><svg
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                <path
+                                    d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z">
+                                </path>
+                            </svg></span></button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
+
 </html>
