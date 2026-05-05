c1f2477c4f75e81cdf4d89e90180eb522d9b9bf3
hogantte <doganorhann4021@gmail.com>
2026-05-05 18:45:26 +0300

    Ürün silme işlemi güncellendi; silinen ürünün fotoğrafı da dosya sisteminden kaldırılıyor.

--- urunSil.php
+++ urunSil.php
@@ -11,11 +11,24 @@ if (isset($_GET['id'])) {
 
     $silinecek_id = $_GET['id'];
 
+    $sorgu = $db->prepare("SELECT urun_foto FROM urunler WHERE id = ? and ekleyen_id = ?");
+    $sorgu->execute([$silinecek_id, $_SESSION["kullanici_id"]]);
+    $urun = $sorgu->fetch(PDO::FETCH_ASSOC);
+
+    if ($urun) {
+        $dosyaYolu = "urun-img/" . $urun['urun_foto'];
+
+        if (file_exists($dosyaYolu)) {
+            unlink($dosyaYolu);
+        }
+    }
+
 
     $kontrol = $db->prepare("DELETE FROM urunler WHERE id = ? and ekleyen_id = ?");
     $sonuc = $kontrol->execute([$silinecek_id, $_SESSION["kullanici_id"]]);
 
     if ($sonuc) {
+
         header("Location: urunler.php?durum=silindi");
     } else {
         header("Location: urunler.php?durum=hata");
